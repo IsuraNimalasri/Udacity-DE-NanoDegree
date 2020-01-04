ca925e7b4d078538e48cd63f6fe1e2053634c950
IssaN <isuranimalasiri@gmail.com>
2020-01-04 22:48:11 +0530

    Add new changes to sql queires

--- 01_DataModeling_PostgreSQL_Project01/sql_queries.py
+++ 01_DataModeling_PostgreSQL_Project01/sql_queries.py
@@ -9,31 +9,31 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE "songplays" (
+CREATE TABLE IF NOT EXISTS "songplays" (
   "songplay_id" int PRIMARY KEY,
-  "start_time" TIMESTAMP,
-  "user_id" int,
+  "start_time" TIMESTAMP NOT NULL,
+  "user_id" int NOT NULL REFERENCES users (user_id),
   "level" varchar,
-  "song_id" varchar,
-  "artist_id" varchar,
-  "session_id" int,
+  "song_id" varchar REFERENCES songs (song_id),
+  "artist_id" varchar REFERENCES artist (artist_id),
+  "session_id" int NOT NULL,
   "location" varchar,
   "user_agent" varchar
 );
 """)
 
 user_table_create = ("""
-CREATE TABLE "users" (
+CREATE TABLE IF NOT EXISTS "users" (
   "user_id" int PRIMARY KEY,
-  "first_name" varchar,
-  "last_name" varchar,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
   "gender" varchar,
   "level" varchar
 );
 """)
 
 song_table_create = ("""
-CREATE TABLE "songs" (
+CREATE TABLE IF NOT EXISTS "songs" (
   "song_id" varchar PRIMARY KEY,
   "title" varchar,
   "artist_id" varchar,
@@ -43,7 +43,7 @@ CREATE TABLE "songs" (
 """)
 
 artist_table_create = ("""
-CREATE TABLE "artist" (
+CREATE TABLE IF NOT EXISTS "artist" (
   "artist_id" varchar PRIMARY KEY,
   "name" varchar,
   "location" varchar,
@@ -53,8 +53,8 @@ CREATE TABLE "artist" (
 """)
 
 time_table_create = ("""
-CREATE TABLE "time" (
-  "start_time" TIMESTAMP PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS "time" (
+  "start_time" TIMESTAMP NOT NULL PRIMARY KEY,
   "hour" int,
   "day" int,
   "week" int,
@@ -72,7 +72,7 @@ ON CONFLICT (songplay_id) DO NOTHING;""")
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name,gender,level) VALUES (%s, %s, %s,%s, %s)
-ON CONFLICT (user_id) DO NOTHING;
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""
@@ -99,5 +99,5 @@ SELECT songs.song_id, songs.artist_id FROM(songs JOIN artist ON songs.artist_id 
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [ user_table_create, song_table_create, artist_table_create, time_table_create,songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
